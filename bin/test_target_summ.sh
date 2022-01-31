eef857719ad6dba6f83ff14d9db8ef4ba334c95c
nhorner <neil.horner@nanoporetech.com>
2022-01-31 16:32:01 +0000

    Fixing plots and tables

--- bin/test_target_summ.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-aln="/Users/Neil.Horner/work/testing/cas9/test_data/fastq_pass.bed"
-targets="/Users/Neil.Horner/work/workflow_outputs/cas9/targets.bed"
-OUTDIR="/Users/Neil.Horner/work/workflow_outputs/cas9/grch38/outtest"
-stats="/Users/Neil.Horner/work/workflow_outputs/cas9/seqstats.csv"
-
-bedtools coverage -a targets -b aln > $OUTDIR/target_summary.bed
